dc02fcd0c04cef0ed9b2f69542381634c4535a0b
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-26 12:23:43 -0400

    Add Login with Protected Endpoint
    Comment
    
    Added login feature with protected route endpoint with bcrypt & JWT

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,15 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
 const db = require('../database/dbConfig');
-const jwt = require('jsonwebtoken');
-const jwtKey = require('../_secrets/keys').jwtKey;
 const { authenticate, generateToken } = require('./middlewares');
+
 
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
 };
+
 
 function register(req, res) {
   // implement user registration		
@@ -34,9 +34,26 @@ function register(req, res) {
 		});
 }	
 
+
 function login(req, res) {
-  // implement user login
-}
+  // implement user login		
+  const credentials = req.body;
+
+ 	db('users')
+		.where({ username: credentials.username })
+		.then(([user]) => {
+			if (user && bcrypt.compareSync(credentials.password, user.password)) {
+				const token = generateToken(user);
+				res
+					.status(200)
+					.json({ message: `User ${credentials.username} logged in`, token });
+			} else {
+				res.status(401).json({ error: 'Not authorized' });
+			}
+		})
+		.catch(err => res.status(500).json(err));
+}	
+
 
 function getJokes(req, res) {
   axios
